5e0bae5a8bc89a1f8662dd4da7e39e31aeec7146
Elodie-mbaye <9992@holbertonstudents.com>
2025-01-16 10:12:47 +0100

    print arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) > 1:
-    for i in range(len(sys.argv)):
-        print(sys.argv[i])
-else:
-    print("Aucun argument n'a été passé au script.")
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
+
